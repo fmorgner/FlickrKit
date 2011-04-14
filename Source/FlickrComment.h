53cb0f9a3b5cda4666968805677005ed3c07c352
Felix Morgner <felix.morgner@gmail.com>
2011-04-14 16:34:26 +0200

    added FlickrComment class

---
+++ Source/FlickrComment.h
@@ -0,0 +1,17 @@
+//
+//  FlickrComment.h
+//  FlickrKit
+//
+//  Created by Felix Morgner on 27.03.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FlickrComment : NSObject
+	{
+    
+	}
+
+@end
---
+++ Source/FlickrComment.m
@@ -0,0 +1,39 @@
+//
+//  FlickrComment.m
+//  FlickrKit
+//
+//  Created by Felix Morgner on 27.03.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import "FlickrComment.h"
+
+
+@implementation FlickrComment
+
+- (id)init
+	{	
+	if ((self = [super init]))
+		{
+  	
+		}
+    
+	return self;
+	}
+
+- (id)initWithXMLElement:(NSXMLElement*)anElement
+	{	
+	if ((self = [super init]))
+		{
+  	
+		}
+    
+	return self;
+	}
+
+- (void)dealloc
+	{
+  [super dealloc];
+	}
+
+@end
