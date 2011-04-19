957e4c6ab714629ae623fd722f87e09b35ea0940
Felix Morgner <felix.morgner@gmail.com>
2011-04-19 18:51:24 +0200

    fixed some placeholders

--- Source/FlickrKitConstants.h
+++ Source/FlickrKitConstants.h
@@ -6,7 +6,7 @@
 //  Copyright 2011 Felix Morgner. All rights reserved.
 //
 
-static NSString* FlickrAPIBaseURL = @"http://api.flickr.com/services/rest/?method=%@";
+static NSString* FlickrAPIBaseURL = @"http://api.flickr.com/services/rest/?method=%@&api_key=%@";
 
 
 
@@ -17,3 +17,4 @@ static NSString* FlickrAPIMethodPhotosGetFavorites = @"flickr.photos.getFavorite
 static NSString* FlickrAPIMethodPhotosGetSizes = @"flickr.photos.getSizes";
 static NSString* FlickrAPIMethodGalleriesGetListForPhoto = @"flickr.galleries.getListForPhoto";
 static NSString* FlickrAPIMethodPeopleGetInfo = @"flickr.people.getInfo";
+static NSString* FlickrAPIMethodPhotosGetEXIF = @"flickr.photos.getExif&photo_id=%@";
