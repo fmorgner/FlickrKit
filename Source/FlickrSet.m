d4ebdfead1b7fbd9957656b2322a953fb15460e2
Felix Morgner <felix.morgner@gmail.com>
2011-04-23 23:27:37 +0200

    added FlickrSet class

---
+++ Source/FlickrSet.m
@@ -0,0 +1,69 @@
+//
+//  FlickrSet.m
+//  FlickrKit
+//
+//  Created by Felix Morgner on 23.04.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import "FlickrSet.h"
+#import "FlickrPersonManager.h"
+
+@implementation FlickrSet
+
+@synthesize ID;
+@synthesize primary;
+@synthesize title;
+@synthesize desc;
+@synthesize owner;
+@synthesize photoCount;
+
+- (id)initWithXMLElement:(NSXMLElement*)anElement
+	{
+	if ((self = [super init]))
+		{
+		self.ID = [[anElement attributeForName:@"id"] stringValue];
+		self.title = [[[anElement elementsForName:@"title"] lastObject] stringValue];
+
+		FlickrPerson* searchResult = [[FlickrPersonManager sharedManager] personForID:[[anElement attributeForName:@"owner"] stringValue]];
+		
+		if (searchResult)
+			{
+			self.owner = searchResult;
+  		}
+		else
+			{
+			self.owner = [FlickrPerson personWithID:[[anElement attributeForName:@"author"] stringValue]];
+			[[FlickrPersonManager sharedManager] addPerson:self.owner];
+			}
+    }
+	return self;
+	}
+
+- (id)initWithID:(NSString*)anID title:(NSString*)aTitle
+	{
+	if((self = [super init]))
+		{
+		self.ID = anID;
+		self.title = aTitle;
+		}
+	
+	return self;
+	}
+
++ (FlickrSet*)setWithXMLElement:(NSXMLElement*)anElement
+	{
+	return [[[FlickrSet alloc] initWithXMLElement:anElement] autorelease];
+	}
+	
++ (FlickrSet*)setWithID:(NSString*)anID title:(NSString*)atitle
+	{
+	return [[[FlickrSet alloc] initWithID:anID title:atitle] autorelease];
+	}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
