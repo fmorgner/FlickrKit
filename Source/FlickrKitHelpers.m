513bbef93f20616e937d28fb6eb70293f9beb98e
Felix Morgner <felix.morgner@gmail.com>
2011-04-23 22:15:20 +0200

    added a new file implementing the "flickrImageSizeString(FlickrImageSize size)" helper function

---
+++ Source/FlickrKitHelpers.m
@@ -0,0 +1,44 @@
+//
+//  FlickrKitHelpers.c
+//  FlickrKit
+//
+//  Created by Felix Morgner on 23.04.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FlickrKitConstants.h"
+
+NSString* flickrImageSizeString(FlickrImageSize size)
+	{
+	NSString* returnString = nil;
+
+	switch (size)
+		{
+  case FlickrImageSizeSquare:
+		returnString = @"Square";    
+    break;
+  case FlickrImageSizeThumbnail:
+		returnString = @"Thumbnail";    
+    break;
+  case FlickrImageSizeSmall:
+		returnString = @"Small";    
+    break;
+  case FlickrImageSizeMedium:
+		returnString = @"Medium";    
+    break;
+  case FlickrImageSizeMedium640:
+		returnString = @"Medium 640";    
+    break;
+  case FlickrImageSizeLarge:
+		returnString = @"Large";    
+    break;
+  case FlickrImageSizeOriginal:
+		returnString = @"Original";    
+    break;
+  default:
+    break;
+		}
+	
+	return returnString;
+	}
