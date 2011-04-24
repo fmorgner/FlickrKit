b3deb7c49a7a7f9b458f5086da2ab9d6330e2fa4
Felix Morgner <felix.morgner@gmail.com>
2011-04-24 13:20:23 +0200

    added some comments to make things a little clearer

--- Source/FlickrKitConstants.h
+++ Source/FlickrKitConstants.h
@@ -6,18 +6,65 @@
 //  Copyright 2011 Felix Morgner. All rights reserved.
 //
 
+// This macro allows us to easily retrieve the API key in any place of the framework
+
 #define APIKey [(NSObject*)[NSApp delegate] valueForKey:@"apiKey"]
+
+// ---- API call URL stuff ---- //
+
+/* 
+ * BASICS
+ * 
+ * There are some basic things to know about API calls.
+ * for each API call the current user must have the
+ * permissions to access the requested information.
+ * This means, that if no user is authenticated,
+ * the information must be public, or otherwise can't
+ * be fetched.
+ *
+ * If no information is to be fetched, the user must
+ * have the permission to change the requested sttribute
+ * or else the call will fail. Some method calls must be
+ * singned with a user token.
+ * 
+ */
+
+// This is the base url for every API call. Every call needs to at least
+// have a method and the api_key of the calling application
 
 static NSString* FlickrAPIBaseURL = @"http://api.flickr.com/services/rest/?method=%@&api_key=%@";
 
+// - flickr.photos.* methods
+
+// This method is used to gather some basic information about a photo.
 static NSString* FlickrAPIMethodPhotosGetInfo = @"flickr.photos.getInfo&photo_id=%@";
+
+// This method is used to gather all visible pools and sets a photo belongs to.
 static NSString* FlickrAPIMethodPhotosGetAllContexts = @"flickr.photos.getAllContexts&photo_id=%@";
+
+// This method is used to gather all comments on a photo.
 static NSString* FlickrAPIMethodPhotosCommentsGetList = @"flickr.photos.comments.getList&photo_id=%@";
+
+// This method is used to gather a list of people who favorited a photo.
 static NSString* FlickrAPIMethodPhotosGetFavorites = @"flickr.photos.getFavorites&photo_id=%@";
+
+// This method is used to gather a list of URLs for available sizes of a photo.
 static NSString* FlickrAPIMethodPhotosGetSizes = @"flickr.photos.getSizes&photo_id=%@";
-static NSString* FlickrAPIMethodGalleriesGetListForPhoto = @"flickr.galleries.getListForPhoto&photo_id=%@";
+
+// This method is used to gather the EXIF information of a photo.
 static NSString* FlickrAPIMethodPhotosGetEXIF = @"flickr.photos.getExif&photo_id=%@";
+
+// - flickr.galleries.* methods
+
+// This method is used to gather a list of galleries a photo appears in.
+static NSString* FlickrAPIMethodGalleriesGetListForPhoto = @"flickr.galleries.getListForPhoto&photo_id=%@";
+
+// - flickr.people.* methods
+
+// This method is used to gather some information about a person.
 static NSString* FlickrAPIMethodPeopleGetInfo = @"flickr.people.getInfo&user_id=%@";
+
+// ---- Image size stuff ---- //
 
 // FlickrImageSize enumeration: represents the sizes of images you can fetch from flickr.
 
@@ -31,5 +78,8 @@ typedef enum _FlickrImageSize
 	FlickrImageSizeLarge = 32,
 	FlickrImageSizeOriginal = 64,
 	} FlickrImageSize;
+	
+// Returns an NSString object representing the name of the given FlickrImageSize.
+// This can be very useful to formulate and xpath query.
 
 extern NSString* flickrImageSizeString(FlickrImageSize size);
