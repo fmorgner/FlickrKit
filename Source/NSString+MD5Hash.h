3e67d72db64662374e5d9c0ad3e4c0a42318366d
Felix Morgner <felix.morgner@gmail.com>
2011-04-25 15:53:35 +0200

    added MD5 hashing category on NSString

---
+++ Source/NSString+MD5Hash.h
@@ -0,0 +1,16 @@
+//
+//  MD5Hash.h
+//  FlickrKit
+//
+//  Created by Felix Morgner on 25.04.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (MD5Hash)
+
+- (NSString *) MD5Hash;
+
+@end
