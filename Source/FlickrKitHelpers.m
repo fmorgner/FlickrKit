55bfa62ee24b63cff263c094efafc667f7cf93b0
Felix Morgner <felix.morgner@gmail.com>
2011-04-25 15:52:50 +0200

    basic implementation of the URL creation function

--- Source/FlickrKitHelpers.m
+++ Source/FlickrKitHelpers.m
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "FlickrKitConstants.h"
 #import "FlickrKitResourceManager.h"
+#import "NSString+MD5Hash.h"
 
 NSString* flickrImageSizeString(FlickrImageSize size)
 	{
@@ -77,4 +78,38 @@ NSString* flickrImageSizeLocalizedString(FlickrImageSize size)
 		}
 	
 	return returnString;
+	}
+
+NSURL* flickrMethodURL(NSString* method, NSDictionary* arguments, BOOL sign)
+	{
+	NSMutableString* argumentString = [NSMutableString string];
+	NSMutableString* urlString = [NSMutableString stringWithFormat:FlickrAPIBaseURL, method, APIKey];
+
+	NSArray* sortedKeys = [[arguments allKeys] sortedArrayUsingSelector:@selector(compare:)];
+
+	for(NSString* key in sortedKeys)
+		{
+		NSString* appendString = [NSString stringWithFormat:@"&%@=%@", key, [arguments valueForKey:key]];
+		[argumentString appendString:appendString];
+		}
+	[urlString appendString:argumentString];
+	
+	NSMutableDictionary* signatureArgumentSet = [NSMutableDictionary dictionaryWithDictionary:arguments];
+	[signatureArgumentSet setObject:method forKey:@"method"];
+	[signatureArgumentSet setObject:APIKey forKey:@"api_key"];
+
+	sortedKeys = [[signatureArgumentSet allKeys] sortedArrayUsingSelector:@selector(compare:)];
+	NSMutableString* signatureBaseString = [NSMutableString stringWithFormat:@"%@", APISecret];
+
+	for(NSString* key in sortedKeys)
+		{
+		NSString* appendString = [NSString stringWithFormat:@"%@%@", key, [signatureArgumentSet valueForKey:key]];
+		[signatureBaseString appendString:appendString];
+		}
+	
+	NSString* signature = [[signatureBaseString MD5Hash] lowercaseString];
+	
+	[urlString appendFormat:@"&api_sig=%@", signature];
+	
+	return [NSURL URLWithString:urlString];
 	}
