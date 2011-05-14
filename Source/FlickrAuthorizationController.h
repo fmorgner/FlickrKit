e7f09393a952860dcd2af5bd7e87435b03f2d171
Felix Morgner <felix.morgner@gmail.com>
2011-05-14 15:55:30 +0200

    added FlickrAuthorizationController class

---
+++ Source/FlickrAuthorizationController.h
@@ -0,0 +1,17 @@
+//
+//  FlickrAuthorizationController.h
+//  FlickrKit
+//
+//  Created by Felix Morgner on 14.05.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FlickrAuthorizationController : NSObject {
+@private
+    
+}
+
+@end
---
+++ Source/FlickrAuthorizationController.m
@@ -0,0 +1,29 @@
+//
+//  FlickrAuthorizationController.m
+//  FlickrKit
+//
+//  Created by Felix Morgner on 14.05.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import "FlickrAuthorizationController.h"
+
+
+@implementation FlickrAuthorizationController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
