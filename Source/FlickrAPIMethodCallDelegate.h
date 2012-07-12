5185e484b547c46b22a879ee2c9d481853a36bf3
Felix Morgner <felix.morgner@gmail.com>
2012-07-12 21:51:56 +0200

    renamed FlickrAPIRequestDelegate to FlickrAPIMethodCallDelegate

---
+++ Source/FlickrAPIMethodCallDelegate.h
@@ -0,0 +1,18 @@
+//
+//  FlickrAPIRequestDelegate.h
+//  FlickrKit
+//
+//  Created by Felix Morgner on 08.07.12.
+//  Copyright (c) 2012 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class FlickrAPIMethodCall;
+
+@protocol FlickrAPIMethodCallDelegate <NSObject>
+
+- (void)requestDidFinish:(FlickrAPIMethodCall*)theRequest;
+- (void)request:(FlickrAPIMethodCall*)theRequest didFailWithError:(NSError*)theError;
+
+@end
